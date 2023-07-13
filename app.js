b5d018ba854cf3a8676959538cc0cefead1021b7
Jeff F <jeff@jrfink.net>
2023-07-12 21:06:27 -0400

    change fallback for process env port

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ app.post('/login', (req, response) => {
     con.end();
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 
 app.listen(port, () => {
     console.log(`server started on port ${port}`);
